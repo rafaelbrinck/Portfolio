0174fba152c637780193816301675abc0d443a9b
Rafael Brinckmann <rafabrinckmann@gmail.com>
2025-07-20 23:05:49 -0300

    Animação no iniciar

--- src/app/components/shared/barra-iniciar/barra-iniciar.ts
+++ src/app/components/shared/barra-iniciar/barra-iniciar.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 import { ModalSkill } from '../../../services/modal-skill';
 import { ModalSobre } from '../../../services/modal-sobre';
 import { ModalProject } from '../../../services/modal-project';
 import { ModalIniciar } from '../../../services/modal-iniciar';
+import { animate } from 'motion';
 
 @Component({
   selector: 'app-barra-iniciar',
@@ -11,12 +12,41 @@ import { ModalIniciar } from '../../../services/modal-iniciar';
   styleUrl: './barra-iniciar.css',
 })
 export class BarraIniciar {
+  @ViewChild('modalContainer', { static: false }) modalContainer!: ElementRef;
   constructor(
+    private renderer: Renderer2,
     private modalSkill: ModalSkill,
     private modalSobre: ModalSobre,
     private modalProjects: ModalProject,
     private modalIniciar: ModalIniciar
   ) {}
+
+  ngAfterViewInit() {
+    if (this.modalContainer?.nativeElement) {
+      const element = this.modalContainer.nativeElement;
+      this.renderer.setStyle(element, 'overflow', 'hidden');
+      this.renderer.setStyle(element, 'height', '0');
+      this.renderer.setStyle(element, 'opacity', '0');
+
+      const fullHeight = '300px';
+
+      animate(
+        element,
+        {
+          height: [0, fullHeight],
+          opacity: [0, 1],
+        },
+        {
+          duration: 0.4,
+          ease: 'easeOut',
+        }
+      ).finished.then(() => {
+        // Remove o overflow/height fixo após a animação
+        this.renderer.setStyle(element, 'height', '300px');
+        this.renderer.removeStyle(element, 'overflow');
+      });
+    }
+  }
 
   abrirSkills() {
     if (this.modalSobre.getValue()) {
